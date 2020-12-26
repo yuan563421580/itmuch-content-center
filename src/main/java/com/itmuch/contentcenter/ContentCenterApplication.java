c42d832b02365617e6d175160c762161a00ce137
yuansb <yuansb@asiainfo.com>
2020-12-26 16:28:14 +0800

    [#] 健康检查内容补充

--- src/main/java/com/itmuch/contentcenter/ContentCenterApplication.java
+++ src/main/java/com/itmuch/contentcenter/ContentCenterApplication.java
@@ -641,9 +641,15 @@ public class ContentCenterApplication {
      *      具体请查看手记及官方文档学习使用。
      *
      * Spring Cloud Stream 监控：
+     *  http://localhost:8010/actuator
+     *      http://localhost:8010/actuator/bindings
+     *      http://localhost:8010/actuator/channels ： 其实就是 output
+     *  http://localhost:8010/actuator/health
      *
      * Spring Cloud Stream 异常处理：
      * · 手记：https://www.imooc.com/article/290435
+     * TODO 后续可以尝试进行RetryTemplate重试配置实现  <input channel名称>: 分析应该是在消息生产者实现配置 使用的消费者的group名称？？？
+     * 全局
      *
      * ---
      *
@@ -682,8 +688,28 @@ public class ContentCenterApplication {
      * Spring Cloud Stream 知识盘点：
      * · 手记：https://www.imooc.com/article/290489
      *
+     */
+
+    /**
+     * Spring Cloud Gateway :
+     * · 是Spring Cloud的网关（第二代），未来会取代Zuul（第一代）
+     * · 基于 Netty 、 Reactor 以及 WebFlux 构建
+     *     ~ Netty ：网络通信框架，可以实现高性能的服务端和客户端
+     *     ~ Reactor ：是一个 Reactive 编程模型的实现，正在越来越流行
+     *     ~ WebFlux ：是一个 Reactive 的 Web 框架
+     * · 优点：
+     *     ~ 性能强劲 ：是第一代网关Zuul 1.x的1.6倍！性能PK : https://www.imooc.com/article/285068
+     *     ~ 功能强大 ：内置了很多实用功能，比如转发、监控、限流等
+     *     ~ 设置优雅，易扩展
+     * · 缺点：
+     *     ~ 依赖 Netty 与 WebFlux , 不是 Servlet 编程模型，有一定的适应成本
+     *     ~ 不能在 Servlet 容器下工作，也不能构建成 WAR 包
+     *     ~ 不支持 Spring Boot 1.x
+     * · 转发规则：
+     *     访问 ${GATEWAY_URL}/{微服务X}/** 会转发到 微服务X的/**路径
      *
-     *
+     * 创建网关 gateway 工程。
+     *      后续 Spring Cloud Gateway 相关知识点请在 gateway 工程查看。
      */
 
 }
